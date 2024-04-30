d70762049054c94c3e49abfa7cb8ac904ca3be58
james thorley <jamesthorley291@gmail.com>
2024-04-30 11:11:20 +0100

    adding server side for database

--- server.js
+++ server.js
@@ -9,9 +9,15 @@ const username = 'jamesthorley291';
 const password = encodeURIComponent('iMiiCas150Penena');
 
 // Models
-const User = require('./models/User');
-const Book = require('./models/Book');
-const Message = require('./models/Message');
+const Meeting = require('./models/Meeting');
+const PersonalSupervisor = require('./models/Personalsupervisor');
+const SelfReport = require('./models/Selfreport');
+const SeniorTutor = require('./models/Seniortutor');
+const Relationship = require('./models/Relationship');
+const Student = require('./models/Student');
+
+
+
 
 const mongoUri = `mongodb+srv://${username}:${password}@cluster0.qvjhqe8.mongodb.net/`;
 const PORT = process.env.PORT || 3000;
@@ -47,26 +53,58 @@ const isAuth = (req, res, next) => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-// User login
 app.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email: email.trim() });
-  if (!user) {
-    return res.redirect('/login');
+  const { username, password, userType } = req.body;
+
+  let model;
+  switch (userType) {
+    case 'student':
+      model = Student;
+      break;
+    case 'personalSupervisor':
+      model = PersonalSupervisor;
+      break;
+    case 'seniorTutor':
+      model = SeniorTutor;
+      break;
+    default:
+      return res.status(400).send("Invalid user type");  // Handling unexpected user types
   }
-  const isMatch = await bcrypt.compare(password.trim(), user.password);
-  if (!isMatch) {
-    return res.redirect("/login");
+
+  try {
+    const user = await model.findOne({ username: username.trim() });
+    if (!user) {
+      return res.redirect('/login');  // User not found, redirect to login
+    }
+    const isMatch = await bcrypt.compare(password.trim(), user.passwordHash);
+    if (!isMatch) {
+      return res.redirect("/login");  // Password does not match, redirect to login
+    }
+    req.session.isAuth = true;
+    req.session.userId = user._id;
+    req.session.userType = userType;  // Store user type in session
+
+    // Redirect based on user type
+    switch (userType) {
+      case 'student':
+        res.redirect('/studentDashboard');
+        break;
+      case 'personalSupervisor':
+        res.redirect('/supervisorDashboard');
+        break;
+      case 'seniorTutor':
+        res.redirect('/seniorTutorDashboard');
+        break;
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Login error");
   }
-  req.session.isAuth = true;
-  req.session.userId = user._id;
-  res.redirect('/home');
 });
 
 // User logout
@@ -83,6 +121,30 @@ app.post('/logout', (req, res) => {
 });
 
 // Serve HTML files
+app.get('/studentDashboard', isAuth, (req, res) => {
+  if (req.session.userType === 'student') {
+    res.sendFile(path.join(__dirname, 'views', 'studentDashboard.html'));
+  } else {
+    res.status(403).send("Access Denied");
+  }
+});
+
+app.get('/supervisorDashboard', isAuth, (req, res) => {
+  if (req.session.userType === 'personalSupervisor') {
+    res.sendFile(path.join(__dirname, 'views', 'supervisorDashboard.html'));
+  } else {
+    res.status(403).send("Access Denied");
+  }
+});
+
+app.get('/seniorTutorDashboard', isAuth, (req, res) => {
+  if (req.session.userType === 'seniorTutor') {
+    res.sendFile(path.join(__dirname, 'views', 'seniorTutorDashboard.html'));
+  } else {
+    res.status(403).send("Access Denied");
+  }
+});
+
 
 
 
